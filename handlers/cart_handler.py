2dbbead725e9dca6f2fca9d8f36334d3f867b4eb
Megan Ford <meg.c.ford@gmail.com>
2016-08-09 14:09:46 -0500

    added docstrings.

--- handlers/cart_handler.py
+++ handlers/cart_handler.py
@@ -2,6 +2,11 @@ from utility.utility import *
 
 
 def add_item_to_cart(file_name, user_id, product_id, qty):
+    """
+    Adds a product ID and quantity to a given user's cart. If the product ID already exists with a quantity in the cart, adds the two quantities together.
+    ==========
+    Method Arguments: 1. file name of customer list to deserialize. 2. ID of user whose cart to update. 3. product ID of product to add. 4. quantity of product to add.
+    """
 
     customer_lib = deserialize(file_name)
     try:
@@ -13,6 +18,11 @@ def add_item_to_cart(file_name, user_id, product_id, qty):
 
 
 def delete_cart(file_name, user_id):
+    """
+    Reverts the cart object on the selected user to empty.
+    =========
+    Method Arguments: 1. file name of customer list to deserialize. 2. ID of user whose cart to clear.
+    """
 
     customer_lib = deserialize(file_name)
     customer_lib[user_id].cart = {}
