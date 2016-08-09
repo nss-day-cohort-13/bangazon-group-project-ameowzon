3ea662b838b9ad124956849ed777bd523bcb29fb
Joseph Kane <josephckane@gmail.com>
2016-08-09 14:41:36 -0500

    generate pop report test on hold

--- handlers/line_item_handler.py
+++ handlers/line_item_handler.py
@@ -3,9 +3,21 @@ from utility.utility import *
 
 
 def generate_new_line_item(file, order_id, product_id):
+	"""
+	Creates a new line item object
+
+	Args-file name, order ID that line item is associated with, product ID that line item associated with
+	"""
 	new_li = Line_Item_Object(order_id, product_id)
 	new_li_id = add_to_file(file, new_li)
 	return new_li_id
 
-def build_line_item_dict():
-    pass
+def generate_popularity_report(file):
+	"""
+	Generates a report that lists how many times a product was bought, by how many customers,
+	and how much money each has brought in
+
+	Args-file name
+	"""
+	lib = deserialize(file)
+	li_dict = {obj.product_id: 0 for uid, obj in lib.items()}
