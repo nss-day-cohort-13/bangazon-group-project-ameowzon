76caddd6f8c5f952d3e8805087ce2b8829f25dc2
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-09 14:29:05 -0500

    Fixed indentatin issue in order handler

--- handlers/order_handler.py
+++ handlers/order_handler.py
@@ -3,33 +3,33 @@ from utility.utility import *
 
 
 def new_order(cid, pid, file='data/orders.txt'):
-  """ Creates a new order object with the customer ID and payment
-      passed in
-      Method arguments
-      ================
-      cid - the unique customer ID
-      pid - the unique payment ID
-  """
-  new_order_obj = Order(cid, pid)
-  new_id = add_to_file(file, new_order_obj)
-  return new_id
+    """ Creates a new order object with the customer ID and payment
+        passed in
+        Method arguments
+        ================
+        cid - the unique customer ID
+        pid - the unique payment ID
+    """
+    new_order_obj = Order(cid, pid)
+    new_id = add_to_file(file, new_order_obj)
+    return new_id
 
 
 def build_order_dict(cid=None, file='data/orders.txt'):
-  """ Returns a dictionary of orders matching the arguments passed in
+    """ Returns a dictionary of orders matching the arguments passed in
       Method arguments
       ================
       file - the file to read from; default is the standard orders file,
              but in testing, the test file is passed in
       cid -  the customer ID to match; default is none, which will return
              all customers
-  """
+    """
     order_lib = deserialize(file)
     if cid == None:
-      return order_lib
+    return order_lib
     else:
-      filtered_order_lib = dict()
-      for key, item in order_lib.items():
-        if item.customer_id == cid:
-          filtered_order_lib[key] = item
-      return filtered_order_lib
+    filtered_order_lib = dict()
+    for key, item in order_lib.items():
+      if item.customer_id == cid:
+        filtered_order_lib[key] = item
+    return filtered_order_lib
