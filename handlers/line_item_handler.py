08984d91516f97728c6710482bd00c4f8f06d9c4
Joseph Kane <josephckane@gmail.com>
2016-08-09 13:36:33 -0500

    initial line item methods and tests

--- handlers/line_item_handler.py
+++ handlers/line_item_handler.py
@@ -2,9 +2,10 @@ from objects.line_item_object import *
 from utility.utility import *
 
 
-def new_line_item():
-    pass
-
+def generate_new_line_item(file, order_id, product_id):
+	new_li = Line_Item_Object(order_id, product_id)
+	new_li_id = add_to_file(file, new_li)
+	return new_li_id
 
 def build_line_item_dict():
     pass
