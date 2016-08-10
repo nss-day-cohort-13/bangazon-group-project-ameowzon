4d3c276eb80fc2cc9cf55138253977d919f1cb7e
Joseph Kane <josephckane@gmail.com>
2016-08-10 11:43:19 -0500

    add comments, remove unused code

--- handlers/line_item_handler.py
+++ handlers/line_item_handler.py
@@ -1,5 +1,3 @@
-import locale
-import reprlib
 from objects.line_item_object import *
 from utility.utility import *
 
@@ -22,13 +20,6 @@ def generate_popularity_report():
 	Args-None
 	"""
 	# initial method setup
-	r_products = reprlib.Repr()
-	r_products.maxstring = 17
-	r_order_cust = reprlib.Repr()
-	r_order_cust.maxstring = 11
-	r_revenue = reprlib.Repr()
-	r_revenue.maxstring = 15
-	local.setlocale(locale.LC_ALL, '')
 	li_lib = deserialize("line_item.txt")
 	orders_lib = deserialize("orders.txt")
 	products_lib = deserialize("products.txt")
@@ -54,29 +45,36 @@ def generate_popularity_report():
 	print("*" * 55)
 	order_list, customer_list, revenue_list = [],[],[]
 	for product, info in li_dict.items():
+		# set product names
 		product_name = products_lib[product]['name']
 		order = info["qty"]
 		customers = info["customers"]
 		revenue = info["revenue"]
 
+		# add values to lists (to be used in totals calculation)
 		order_list.append(order)
 		customer_list.append(customer)
 		revenue_list.append(revenue)
 
+		# limit display names/values
 		product_name = (product_name if len(product_name) <= 17 else product_name[:14] + "...") + " "
 		order = (order if len(order) <= 11 else order[:8] + "...") + " "
 		customers = (customers if len(customers) <= 11 else customers[:8] + "...")
 		revenue = (revenue if len(revenue) <= 14 else revenue[:11] + "...")
 
+		# print product info
 		print(row_string.format(product_name, order, customers, revenue))
 	print("*" * 55)
 
+	# calculate totals
 	order_sum = sum(order_list)
 	customer_sum = sum(customer_list)
 	revenue_sum = sum(revenue_list)
 
+	# limit totals display
 	order_sum = (order_sum if len(str(order_sum)) <= 17 else order_sum[:14] + "...") + " "
 	customer_sum = (customer_sum if len(str(customer_sum)) <= 11 else customer_sum[:8] + "...")
 	revenue_sum = (revenue_sum if len(str(revenue_sum)) <= 14 else revenue_sum[:11] + "...")
 
+	# print totals
 	print(total_string.format("Totals:", order_sum, customer_sum, revenue_sum))
