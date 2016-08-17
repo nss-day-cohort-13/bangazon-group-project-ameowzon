502d0ce393a55217db2c911e2227854332f6a525
Joseph Kane <josephckane@gmail.com>
2016-08-17 15:02:48 -0500

    fix sql syntax

--- handlers/order_handler.py
+++ handlers/order_handler.py
@@ -12,7 +12,7 @@ def new_order(cust_key):
         c = conn.cursor()
         c.execute("INSERT INTO Orders (PaymentId, CustomerId) VALUES (?,?)", (None, cust_key))
         conn.commit()
-        c.execute("SELECT o.OrderId FROM Orders o WHERE o.CustomerId=? and o.PaymentId=?", (cust_key, None))
+        c.execute("SELECT o.OrderId FROM Orders o WHERE o.CustomerId=? AND o.PaymentId=?", (cust_key, None))
         order_id = c.fetchone()
         print(order_id[0])
         return order_id[0]
@@ -25,5 +25,5 @@ def add_payment_to_order(cust_key, payment_id):
     """
     with sqlite3.connect("bangazon.db") as conn:
         c = conn.cursor()
-        c.execute("UPDATE Orders SET PaymentId=? WHERE CustomerId=? and PaymentId=?", (payment_id, cust_key, None))
+        c.execute("UPDATE Orders SET PaymentId=? WHERE CustomerId=? AND PaymentId IS ?", (payment_id, cust_key, None))
         conn.commit()
