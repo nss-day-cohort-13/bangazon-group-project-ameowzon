8e594a2d23ab7dcca2adc0b2819c399bcba6fb97
Megan Ford <meg.c.ford@gmail.com>
2016-08-17 12:18:26 -0500

    removed 'add item to cart' function from cart, and added 'build cart'. removed 'generate orders menu' from order handler and added 'add payment to order'. Func names only, no actual implementation.

--- handlers/cart_handler.py
+++ handlers/cart_handler.py
@@ -1,22 +1,6 @@
 from utility.utility import *
 
 
-def add_item_to_cart(file_name, user_id, product_id, qty):
-    """
-    Adds a product ID and quantity to a given user's cart. If the product ID already exists with a quantity in the cart, adds the two quantities together.
-    ==========
-    Method Arguments: 1. file name of customer list to deserialize. 2. ID of user whose cart to update. 3. product ID of product to add. 4. quantity of product to add.
-    """
-
-    customer_lib = deserialize(file_name)
-    try:
-        customer_lib[user_id].cart[product_id] += qty
-    except KeyError:
-        customer_lib[user_id].cart[product_id] = qty
-
-    serialize(file_name, customer_lib)
-
-
 def delete_cart(file_name, user_id):
     """
     Reverts the cart object on the selected user to empty.
@@ -27,3 +11,15 @@ def delete_cart(file_name, user_id):
     customer_lib = deserialize(file_name)
     customer_lib[user_id].cart = {}
     serialize(file_name, customer_lib)
+
+
+def build_cart_view(customer_id):
+
+    with sqlite3.connect("bangazon.db") as database:
+            db = database.cursor()
+
+            db.execute("""select c.CustomerId, c.FullName
+                            from Order o
+                            inner join Customer c on c.CustomerId = o.CustomerId
+                            where o.PaymentId = Null""")
+            return db.fetchall()
