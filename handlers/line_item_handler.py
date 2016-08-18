533afed4fa14b44c50c53494c3741408ba0a7232
Chase Ramsey <ramsey.chase@gmail.com>
2016-08-18 11:21:42 -0500

    Popularity report generation works with SQL

--- handlers/line_item_handler.py
+++ handlers/line_item_handler.py
@@ -29,18 +29,45 @@ def return_report_line_items(input_file='bangazon.db'):
         db = conn.cursor()
         db.execute("""
 SELECT
-    p.ProductId,
     p.Name,
-    COUNT(p.ProductId) 'Units Sold',
+    COUNT(DISTINCT o.OrderId) 'Orders',
     COUNT(DISTINCT c.CustomerId) 'Customers',
     SUM(p.Price) 'Revenue'
 FROM Product p
 INNER JOIN LineItem l ON l.ProductId = p.ProductId
 INNER JOIN Orders o ON l.OrderId = o.OrderId
 INNER JOIN Customer c ON o.CustomerId = c.CustomerId
-WHERE o.PaymentId IS NOT NULL
-GROUP BY p.ProductId
+WHERE o.PaymentId is not NULL
+GROUP BY p.Name
             """)
-        db.commit()
+        conn.commit()
+
+    return db.fetchall()
+
+def return_report_totals(input_file='bangazon.db'):
+    """
+    Returns the totals for the last line of the popularity report
+    """
+    with sqlite3.connect(input_file) as conn:
+        db = conn.cursor()
+        db.execute("""
+SELECT
+    COUNT(DISTINCT TotalOrders) 'Orders',
+    COUNT(DISTINCT TotalCustomers) 'Customers',
+    SUM(TotalRevenue) 'Revenue'
+FROM
+    (SELECT
+        p.Name,
+        o.OrderId as TotalOrders,
+        c.CustomerId as TotalCustomers,
+        p.Price as TotalRevenue
+    FROM Product p
+    INNER JOIN LineItem l ON l.ProductId = p.ProductId
+    INNER JOIN Orders o ON l.OrderId = o.OrderId
+    INNER JOIN Customer c ON o.CustomerId = c.CustomerId
+    WHERE o.PaymentId is not NULL
+    ORDER BY p.Name)
+            """)
+        conn.commit()
 
     return db.fetchall()
