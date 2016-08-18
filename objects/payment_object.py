ea0e61f11848bb9c6ed908a318e2586181fa1be5
Megan Ford <meg.c.ford@gmail.com>
2016-08-18 10:51:38 -0500

    cleaned up utility.py, deleted data and objects folders.

--- objects/payment_object.py
+++
@@ -1,7 +0,0 @@
-
-class Payment_Object():
-
-    def __init__(self, name, account_number, cust_key):
-    	self.name = name
-    	self.account_number = account_number
-    	self.customer = cust_key
--- utility/utility.py
+++ utility/utility.py
@@ -1,18 +1,5 @@
-import pickle
+
 import uuid
-import json
-
-
-def read_json_file(input_file):
-    """ Reads non-binary data from the file passed in as a
-        json and returns the file as a parsed dictionary.
-    """
-    try:
-        with open(input_file, 'r+') as file:
-            return json.load(file)
-    except EOFError:
-        print('{0} is empty.'.format(input_file))
-        return None
 
 
 def get_param(prompt_string, screen):
@@ -33,85 +20,6 @@ def get_param(prompt_string, screen):
     return input
 
 
-def generate_uid():
-    """ Returns a uuid to be used for a dictionary key.
-        ===============
-    """
-    uid = str(uuid.uuid4())
-    return uid
-
-
-def write_json_file(data, output_file):
-    """ Writes the product dictionary to json file.
-        ===============
-        data = dictionary
-        output_file = the place to save the file.
-    """
-    try:
-        with open(output_file, 'w+') as file:
-            json.dump(data, file)
-            return
-    except EOFError:
-        print('{0} is empty.'.format(input_file))
-        return None
-
-
-def deserialize(input_file):
-    """ Reads binary data from the file passed in
-        Method arguments
-        ================
-        input_file - the file to read from
-    """
-    try:
-        with open(input_file, 'rb+') as file:
-            return pickle.load(file)
-    except EOFError:
-        print('{0} is empty.'.format(input_file))
-        return {}
-    except FileNotFoundError:
-        return {}
-
-
-def serialize(output_file, updated_info):
-    """ Writes binary data to the file passed in
-        Method arguments
-        ================
-        updated_info - the complete library to be changed
-        output_file - the file to write to
-    """
-    with open(output_file, 'wb+') as file:
-        pickle.dump(updated_info, file)
-
-
-def get_value(read_file, uid):
-    """ Returns an object from read_file that matches
-        the uid provided as argument
-        Method arguments
-        ================
-        read_file - the file to read from
-        uid - the uid to match
-    """
-    to_search = deserialize(read_file)
-    try:
-        return to_search[uid]
-    except KeyError:
-        return None
-
-
-def add_to_file(write_file, write_value):
-    """ Adds the provided value to the provided file
-        Method arguments
-        ================
-        write_file - the file to write to
-        write_value - the new value to be written
-    """
-    to_write = deserialize(write_file)
-    uid = str(uuid.uuid4())
-    to_write[uid] = write_value
-    # print("to_write[uid] = {0}".format(to_write[uid]))
-    serialize(write_file, to_write)
-    return uid
-
 def print_menu(handler_fn, screen, row_start, cid=None):
     """ Prints a menu from an object list
         Method arguments
@@ -123,7 +31,7 @@ def print_menu(handler_fn, screen, row_start, cid=None):
         row_start - the row you would like to start adding strings
         cid - optional customerID argument
     """
-    if cid != None:
+    if cid is not None:
         temp_list, price = handler_fn(cid)
     else:
         temp_list, price = handler_fn()
@@ -138,6 +46,7 @@ def print_menu(handler_fn, screen, row_start, cid=None):
             row_start += 1
 
     return temp_list
+
 
 def set_thing(temp_list, index):
     """ Returns the UID of the selected item in a temp list
